a7d00b8b39294ec5fc0baa1a439f405d9512768b
tanishqa321 <tender.tanishka@gmail.com>
2025-01-05 01:54:57 +0530

    Added UC1 : feature to find max of 3 float

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class MaxNumber {
+    public float testMax(float num1, float num2, float num3) {
+        float max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+        return max;
+    }
     public int testMax(int num1, int num2, int num3) {
         int max = num1;
         if (num2 > max) {
@@ -19,17 +29,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
+        float num1 = scanner.nextFloat();
 
         System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
+        float num2 = scanner.nextFloat();
 
         System.out.print("Enter the third number: ");
-        int num3 = scanner.nextInt();
+        float num3 = scanner.nextFloat();
 
         MaxNumber maxObj = new MaxNumber();
-        int maxi=maxObj.testMax(num1, num2, num3);
-        System.out.println("Maximum number is "+maxi);
+        float maxi = maxObj.testMax(num1, num2, num3);
+        System.out.println("Maximum number is " + maxi);
+
         scanner.close();
     }
 }
