5a15c40ae566657d323eeefc184a75429e3badd9
tanishqa321 <tender.tanishka@gmail.com>
2025-01-08 01:51:32 +0530

    Added refractor1

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,34 @@
 import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class MaxNumber {
-
-    //UC2
-
-    public float testMax(float num1, float num2, float num3) {
-        float max = num1;
-        if (num2 > max) {
-            max = num2;
-        }
-        if (num3 > max) {
-            max = num3;
-        }
-        return max;
-    }//UC1
-    public int testMax(int num1, int num2, int num3) {
-        int max = num1;
-        if (num2 > max) {
-            max = num2;
-        }
-        if (num3 > max) {
-            max = num3;
-        }
+class MaxValue {
+    public <T extends Comparable<T>> T findMax(T a, T b, T c){
+        T max = a;
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
         return max;
     }
 }
-class MaxString {
-    public String testMax(String str1, String str2, String str3) {
-        String max = str1;
-        if (str2.compareTo(max) > 0) {
-            max = str2;
-        }
-        if (str3.compareTo(max) > 0) {
-            max = str3;
-        }
-        return max;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-/*
-        System.out.print("Enter the first number: ");
-        float num1 = scanner.nextFloat();
+        MaxValue maxFinder = new MaxValue();
 
-        System.out.print("Enter the second number: ");
-        float num2 = scanner.nextFloat();
+        // Strings
+        System.out.print("Enter 3 strings: ");
+        String s1 = sc.next(), s2 = sc.next(), s3 = sc.next();
+        System.out.println("Max String: " + maxFinder.findMax(s1, s2, s3));
 
-        System.out.print("Enter the third number: ");
-        float num3 = scanner.nextFloat();
+        // Integers
+        System.out.print("Enter 3 integers: ");
+        int n1 = sc.nextInt(), n2 = sc.nextInt(), n3 = sc.nextInt();
+        System.out.println("Max Integer: " + maxFinder.findMax(n1, n2, n3));
 
-        MaxNumber maxObj = new MaxNumber();
-        float maxi = maxObj.testMax(num1, num2, num3);
-        System.out.println("Maximum number is " + maxi);
-*/
-        System.out.print("Enter the first string: ");
-        String str1 = scanner.nextLine();
+        // Floats
+        System.out.print("Enter 3 floats: ");
+        float f1 = sc.nextFloat(), f2 = sc.nextFloat(), f3 = sc.nextFloat();
+        System.out.println("Max Float: " + maxFinder.findMax(f1, f2, f3));
 
-        System.out.print("Enter the second string: ");
-        String str2 = scanner.nextLine();
-
-        System.out.print("Enter the third string: ");
-        String str3 = scanner.nextLine();
-
-        MaxString maxObj = new MaxString();
-        String maxString = maxObj.testMax(str1, str2, str3);
-        System.out.println("Maximum string is: " + maxString);
-        scanner.close();
+        sc.close();
     }
 }
