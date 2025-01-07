c001f2b9921cc576d1440c2c2b4cc8e77329280a
tanishqa321 <tender.tanishka@gmail.com>
2025-01-08 01:59:35 +0530

    Added refractor2:generic class created

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class MaxValue {
-    public <T extends Comparable<T>> T findMax(T a, T b, T c){
+// Generic class to hold three variables of a generic type
+class GenericMax<T extends Comparable<T>> {
+    private T a, b, c;
+    public GenericMax(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T testMaximum() {
+        return testMaximum(a, b, c);
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
         T max = a;
         if (b.compareTo(max) > 0) max = b;
         if (c.compareTo(max) > 0) max = c;
@@ -12,23 +24,25 @@ class MaxValue {
 public class Main {
     public static void main(String[] args) {
 
-        MaxValue maxFinder = new MaxValue();
 
+        Scanner sc = new Scanner(System.in);
         // Strings
         System.out.print("Enter 3 strings: ");
         String s1 = sc.next(), s2 = sc.next(), s3 = sc.next();
-        System.out.println("Max String: " + maxFinder.findMax(s1, s2, s3));
+        GenericMax<String> stringMax = new GenericMax<>(s1, s2, s3);
+        System.out.println("Max String: " + stringMax.testMaximum());
 
         // Integers
         System.out.print("Enter 3 integers: ");
         int n1 = sc.nextInt(), n2 = sc.nextInt(), n3 = sc.nextInt();
-        System.out.println("Max Integer: " + maxFinder.findMax(n1, n2, n3));
+        GenericMax<Integer> intMax = new GenericMax<>(n1, n2, n3);
+        System.out.println("Max Integer: " + intMax.testMaximum());
 
         // Floats
         System.out.print("Enter 3 floats: ");
         float f1 = sc.nextFloat(), f2 = sc.nextFloat(), f3 = sc.nextFloat();
-        System.out.println("Max Float: " + maxFinder.findMax(f1, f2, f3));
-
+        GenericMax<Float> floatMax = new GenericMax<>(f1, f2, f3);
+        System.out.println("Max Float: " + floatMax.testMaximum());
         sc.close();
     }
 }
